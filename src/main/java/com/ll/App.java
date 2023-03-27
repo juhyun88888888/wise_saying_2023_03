cf9883e279a27d9291e39e43695acad1b3118a13
juhyun88888888 <lad_agust@naver.com>
2023-03-27 13:48:39 +0900

    work-1

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,2 +1,28 @@
-package com.ll;public class App {
+package com.ll;
+
+import java.util.Scanner;
+
+
+public class App {
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+    }
 }
+
+
+
+
+
